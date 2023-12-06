a5b76d2155809820cd6589eddf238b3a564e8bc5
kmolcard <kevin.molcard@e.email>
2023-12-06 14:23:26 +0100

    remove ARM compilation

--- examples/common/vst3/vst3-macos.mk
+++ examples/common/vst3/vst3-macos.mk
@@ -42,23 +42,23 @@ ${OBJ_DIR_X86_64}/vst3/%.o: ${VST3_SDK_DIR}/%.cpp
 	mkdir -p $(dir $@)
 	${CXX} $^ ${CXXFLAGS} -arch x86_64 -c -o $@
 
-${OBJ_DIR_ARM64}/${NAME}.o: ${ROOT_DIR}/../src/${SOURCE} | ${OBJ_DIR_ARM64}
-	${CXX} ${ROOT_DIR}/../src/${SOURCE} ${CXXFLAGS} -arch arm64 -Wall -Wextra -Wpedantic -c -o $@
+# ${OBJ_DIR_ARM64}/${NAME}.o: ${ROOT_DIR}/../src/${SOURCE} | ${OBJ_DIR_ARM64}
+# 	${CXX} ${ROOT_DIR}/../src/${SOURCE} ${CXXFLAGS} -arch arm64 -Wall -Wextra -Wpedantic -c -o $@
 
-${OBJ_DIR_ARM64}/common/%.o: ${COMMON_DIR}/%.cpp | ${OBJ_DIR_ARM64}/common
-	${CXX} $^ ${CXXFLAGS} -arch arm64 -c -o $@
+# ${OBJ_DIR_ARM64}/common/%.o: ${COMMON_DIR}/%.cpp | ${OBJ_DIR_ARM64}/common
+# 	${CXX} $^ ${CXXFLAGS} -arch arm64 -c -o $@
 
-${OBJ_DIR_ARM64}/vst3/%.o: ${VST3_SDK_DIR}/%.cpp
-	mkdir -p $(dir $@)
-	${CXX} $^ ${CXXFLAGS} -arch arm64 -c -o $@
+# ${OBJ_DIR_ARM64}/vst3/%.o: ${VST3_SDK_DIR}/%.cpp
+# 	mkdir -p $(dir $@)
+# 	${CXX} $^ ${CXXFLAGS} -arch arm64 -c -o $@
 
 build/tmp/${NAME}-x86_64: ${OBJ_COMMON_X86_64} ${OBJ_VST3_SDK_X86_64} ${OBJ_DIR_X86_64}/${NAME}.o | build/tmp
 	${CXX} $^ ${LDFLAGS} -arch x86_64 -o $@
 
-build/tmp/${NAME}-arm64: ${OBJ_COMMON_ARM64} ${OBJ_VST3_SDK_ARM64} ${OBJ_DIR_ARM64}/${NAME}.o | build/tmp
-	${CXX} $^ ${LDFLAGS} -arch arm64 -o $@
+# build/tmp/${NAME}-arm64: ${OBJ_COMMON_ARM64} ${OBJ_VST3_SDK_ARM64} ${OBJ_DIR_ARM64}/${NAME}.o | build/tmp
+# 	${CXX} $^ ${LDFLAGS} -arch arm64 -o $@
 
-${SO_FILE}: build/tmp/${NAME}-x86_64 build/tmp/${NAME}-arm64 | ${SO_DIR}
+${SO_FILE}: build/tmp/${NAME}-x86_64 | ${SO_DIR}
 	lipo -create -output $@ $^
 
 ${BUILD_PLUGIN_DIR}/Contents/Info.plist: ${TEMPLATE_DIR}/Contents/Info.plist | ${BUILD_PLUGIN_DIR}/Contents
